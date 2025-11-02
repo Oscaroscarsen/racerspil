a93edd5064d5a6b575e896822e259f4a7e7a0ab4
Oscaroscarsen <363806@via.dk>
2025-11-02 17:46:31 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -172,7 +172,7 @@ function Gamestart () {
         `)
 }
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
-    info.changeCountdownBy(2)
+    info.changeCountdownBy(-2)
     sprites.destroy(otherSprite)
 })
 let mySprite2: Sprite = null
